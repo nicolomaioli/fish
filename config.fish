f9534f769a0872e8c2f2fdf68a75dcab3e31aed9
Nicolo Maioli <nicolomaioli@gmail.com>
2021-10-24 11:45:54 +0100

    Port over configs from zsh

--- config.fish
+++ config.fish
@@ -1,8 +1,10 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+	set -gx EDITOR nvim
+	set -gx GITHUB_USER nicolomaioli
 
-alias ti='tmux new-session -d'
-alias ta='tmux a -t'
-alias tk='tmux kill-session -t'
-alias tl='tmux list-sessions'
+	alias gmi='go mod init github.com/"$GITHUB_USER"/(basename $PWD)'
+	alias ti='tmux new-session -d'
+	alias ta='tmux a -t'
+	alias tk='tmux kill-session -t'
+	alias tl='tmux list-sessions'
+end
