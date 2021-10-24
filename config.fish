0c01d548febfa9e661cc4eb93f94827678b6ee78
Nicolo Maioli <nicolomaioli@gmail.com>
2021-10-24 12:01:36 +0100

    Source all private configs

--- config.fish
+++ config.fish
@@ -1,10 +1,16 @@
 if status is-interactive
 	set -gx EDITOR nvim
 	set -gx GITHUB_USER nicolomaioli
+	set -gx FISH_CONFIG_FOLDER "$HOME"/.config/fish
 
 	alias gmi='go mod init github.com/"$GITHUB_USER"/(basename $PWD)'
 	alias ti='tmux new-session -d'
 	alias ta='tmux a -t'
 	alias tk='tmux kill-session -t'
 	alias tl='tmux list-sessions'
+
+	# Source private files
+	for file in (ls $FISH_CONFIG_FOLDER/private | grep .fish\$)
+		source $FISH_CONFIG_FOLDER/private/$file
+	end
 end
