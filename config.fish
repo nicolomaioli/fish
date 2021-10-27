ea28c073a6f85194111199033da13f3ea9b8dece
Nicolo Maioli <nicolomaioli@gmail.com>
2021-10-27 14:19:32 +0100

    Fix aliases, add Python packages to path

--- config.fish
+++ config.fish
@@ -1,18 +1,19 @@
 set -gx VISUAL nvim
 set -gx EDITOR nvim
 set -gx GITHUB_USER nicolomaioli
+set -gx FISH_CONFIG_FOLDER $HOME/.config/fish
+
+fish_add_path -a $HOME/Library/Python/3.9/bin
+
+for file in (ls $FISH_CONFIG_FOLDER/private | grep .fish\$)
+	source $FISH_CONFIG_FOLDER/private/$file
+end
 
 if status is-interactive
-	set -gx FISH_CONFIG_FOLDER "$HOME"/.config/fish
-
 	alias gmi='go mod init github.com/"$GITHUB_USER"/(basename $PWD)'
 	alias ti='tmux new-session -d'
-	alias ta='tmux a -t'
-	alias tk='tmux kill-session -t'
 	alias tl='tmux list-sessions'
 
-	# Source private files
-	for file in (ls $FISH_CONFIG_FOLDER/private | grep .fish\$)
-		source $FISH_CONFIG_FOLDER/private/$file
-	end
+	abbr -a ta 'tmux a -t'
+	abbr -a tk 'tmux kill-session -t'
 end
